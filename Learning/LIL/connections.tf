f09d2e4553fa5ede60e17726d04d226ce3d0f794
Your Name <you@example.com>
2019-11-29 22:59:57 +0100

    .

--- Learning/LIL/connections.tf
+++ Learning/LIL/connections.tf
@@ -1,11 +1,25 @@
-provider "vsphere" {
+terraform {
+  required_version = ">= 0.12"
+}
 
-user                 =   ""
-password             =   ""
-vsphere_server       =   ""
-allow_unverified_ssl =   true
+provider "esxi" {
+  esxi_hostname      = "192.168.10.118"
+  esxi_hostport      = "22"
+  esxi_username      = "${var.username}"
+  esxi_password      = "${var.password}"
+}
+
+provider "docker" {
+ host
 
 }
 
+resource "esxi_guest" "VM" {
+  guest_name         = "VM"
+  disk_store         = "MyDiskStore"
 
 
+  network_interfaces {
+    virtual_network = "Nettwork"
+  }
+}
